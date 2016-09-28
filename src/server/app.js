24c6afb91174f87148c0e2dfcd6e363327c6484a
Anthony Turrigiano <anthony.turrigiano@gmail.com>
2016-09-28 15:42:24 +0000

    Add app.js file in src/server directory

---
+++ src/server/app.js
@@ -0,0 +1,22 @@
+//configuration settings
+var config = require(path.resolve("./config/config"));
+
+//path module
+var path = require("path");
+
+//express.js module
+var express = require("express");
+var app = express();
+
+//npm modules
+require("./lib/lib")(app);
+
+//static files
+app.use("/public", express.static(path.resolve(config.src.paths.public)));
+app.use("/client", express.static(path.resolve(config.src.paths.client)));
+
+//routes
+require("./routes/routes")(app);
+
+//export app
+module.exports = app;
