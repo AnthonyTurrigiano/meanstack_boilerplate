fd457a7648b3bbfda331e50df7c6ffd7f6e16615
Anthony Turrigiano <anthony.turrigiano@gmail.com>
2016-09-28 16:21:39 +0000

    Install and configure gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,12 @@ var config = require("./config/config");
 //Require gulp module
 var gulp = require("gulp");
 
+//Require gulp load plugins module
+var plugins = require("gulp-load-plugins")({
+    rename : {"postcss-font-magician" : "fontMagician"},
+    pattern : ["*"]
+});
+
 
 //tasks
+gulp.task("src:styles", require("./gulptasks/src/styles")(gulp, plugins, config));
